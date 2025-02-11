a3d48b515a0ecd962aa6125b656669a471bedc9c
Krystal Gallagher <Kxfusion@gmail.com>
2025-02-11 13:07:06 -0500

    Wrap text with apos in template literal

--- client/src/app/components/empty-table.tsx
+++ client/src/app/components/empty-table.tsx
@@ -4,7 +4,7 @@ export const EmptyTable = () => {
     return (
         <div className="w-[50%] pt-10 m-auto flex flex-column flex-wrap place-content-center">
             <Clipboard />
-            <div className="w-full pt-2.5 text-tlight text-center font-bold">You don't have any tasks.</div>
+            <div className="w-full pt-2.5 text-tlight text-center font-bold">{`You don't have any tasks.`}</div>
             <div className="w-full pt-5 text-tlight text-center">Click the Create Task button to get started!</div>
         </div>
     );
